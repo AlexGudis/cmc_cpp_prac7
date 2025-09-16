fcb9351d2449464983823a69b155d280037e9187
AlexGudis <gudis2003@mail.ru>
2025-09-16 14:11:46 +0300

    test my smart pointer and created logging module

--- mafia/test_shared.cpp
+++ mafia/test_shared.cpp
@@ -4,6 +4,38 @@
 
 int main(void)
 {
-    MySharedPtr<int *> ptr1 = new int{1};
-    return 0
+    {
+        // Test deafault constructor
+        std::cout << "---------- TEST 1 ----------" << std::endl;
+        MySharedPtr<int> ptr1(new int{1});
+        std::cout << ptr1.get() << " : " << *ptr1.get() << std::endl;
+    }
+
+    {
+        // Test copy constructor
+        std::cout << "---------- TEST 2 ----------" << std::endl;
+        MySharedPtr<int> ptr1(new int{1});
+        MySharedPtr<int> ptr2(ptr1);
+        std::cout << ptr1.get() << " : " << *ptr1.get() << std::endl;
+        std::cout << ptr2.get() << " : " << *ptr2.get() << std::endl;
+    }
+
+    {
+        std::cout << "---------- TEST 3 ----------" << std::endl;
+        MySharedPtr<int> ptr1(new int{1});
+        MySharedPtr<int> another_ptr1(new int{2});
+        MySharedPtr<int> ptr2 = ptr1;
+        std::cout << ptr2.get() << " : " << *ptr2.get() << std::endl;
+        ptr2 = another_ptr1;
+        std::cout << ptr1.get() << " : " << *ptr1.get() << std::endl;
+        std::cout << ptr2.get() << " : " << *ptr2.get() << std::endl;
+    }
+
+    {
+        std::cout << "---------- TEST 4 ----------" << std::endl;
+        // Test move constructor
+        MySharedPtr<int> ptr1 = MySharedPtr<int>{new int{4}};
+        std::cout << ptr1.get() << " : " << *ptr1.get() << std::endl;
+    }
+    return 0;
 }
