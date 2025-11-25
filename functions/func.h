29820bb3e257663d19a21e47c6e86237067a2c20
AlexGudis <gudis2003@mail.ru>
2025-11-25 15:22:41 +0300

    refactor

--- functions/func.h
+++ functions/func.h
@@ -6,168 +6,171 @@
 #include <string>
 #include <stdexcept>
 
-// --------------------------------------------------------- Абстрактные методы ------------------------------------------ 
-
+// --------------------------------------------------------- Абстрактные методы ------------------------------------------
 
 // Базовый абстрактный класс TFunction - представляет математическую функцию одной переменной
-class TFunction {
-    public:
-        virtual ~TFunction() = default;
+class TFunction
+{
+public:
+    virtual ~TFunction() = default;
 
-        // Оператор вызова функции - вычисляет значение функции в точке x
-        virtual double operator()(double x) const = 0;
-        
-        // Вычисляет производную функции в точке x
-        virtual double GetDeriv(double x) const = 0;
-        
-        // Возвращает строковое представление функции
-        virtual std::string ToString() const = 0;
-        
-        /**
-         * Создает копию объекта (паттерн Prototype)
-         * Необходим для корректного создания составных функций через операторы
-         */
-        virtual std::shared_ptr<TFunction> Clone() const = 0;
-    };
-    
-    using TFunctionPtr = std::shared_ptr<TFunction>;
-    
+    // Оператор вызова функции - вычисляет значение функции в точке x
+    virtual double operator()(double x) const = 0;
 
+    virtual double GetDeriv(double x) const = 0;
 
-    // ------------------------------------------------------------------- Базовые функции ------------------------------------------------------------------- 
-    // Тождественная функция f(x) = x
-    class IdentFunction : public TFunction {
-    public:
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Константная функция f(x) = c
-    class ConstFunction : public TFunction {
-    private:
-        double value_;  // Значение константы
-    public:
-        explicit ConstFunction(double value);  // explicit запрещает неявное преобразование
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Степенная функция f(x) = x^n
-    class PowerFunction : public TFunction {
-    private:
-        int power_;  // Показатель степени
-    public:
-        explicit PowerFunction(int power);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Экспоненциальная функция f(x) = exp(x)
-    class ExpFunction : public TFunction {
-    public:
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Полиномиальная функция f(x) = a_0 + a_1 * x + a_2 * x^2 + ... + a_n * x^n
-    class PolynomialFunction : public TFunction {
-    private:
-        std::vector<double> coefficients_;  // a_0, a_1, ...
-    public:
-        explicit PolynomialFunction(const std::vector<double>& coefficients);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
+    virtual std::string ToString() const = 0;
 
+    virtual std::shared_ptr<TFunction> Clone() const = 0;
+};
 
-    // -----------------------------  Составные функции (арифм. операции) ---------------------------------------
-    // Функция-сумма: f(x) = g(x) + h(x)
-    class AddFunction : public TFunction {
-    private:
-        TFunctionPtr left_, right_;
-    public:
-        AddFunction(TFunctionPtr left, TFunctionPtr right);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Функция-разность: f(x) = g(x) - h(x)
-    class SubtractFunction : public TFunction {
-    private:
-        TFunctionPtr left_, right_;
-    public:
-        SubtractFunction(TFunctionPtr left, TFunctionPtr right);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Функция-произведение: f(x) = g(x) * h(x)
-    class MultiplyFunction : public TFunction {
-    private:
-        TFunctionPtr left_, right_;
-    public:
-        MultiplyFunction(TFunctionPtr left, TFunctionPtr right);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // Функция-частное: f(x) = g(x) / h(x)
-    class DivideFunction : public TFunction {
-    private:
-        TFunctionPtr left_, right_;
-    public:
-        DivideFunction(TFunctionPtr left, TFunctionPtr right);
-        double operator()(double x) const override;
-        double GetDeriv(double x) const override;
-        std::string ToString() const override;
-        std::shared_ptr<TFunction> Clone() const override;
-    };
-    
-    // ------------------------------------------ Фабрика ------------------------------------------
-    class FunctionFactory {
-    public:
-        /**
-         * Основной метод создания функций
-         * type - тип функции
-         * params - параметры для создания функции
-         */
-        static TFunctionPtr Create(const std::string& type, const std::vector<double>& params = {});
-        
-        // Перегруженные версии для удобства
-        static TFunctionPtr Create(const std::string& type, double param);
-        static TFunctionPtr Create(const std::string& type, int param);
-    };
-    
-    // Перегрузка операторов
-    TFunctionPtr operator+(const TFunction& lhs, const TFunction& rhs);
-    TFunctionPtr operator-(const TFunction& lhs, const TFunction& rhs);
-    TFunctionPtr operator*(const TFunction& lhs, const TFunction& rhs);
-    TFunctionPtr operator/(const TFunction& lhs, const TFunction& rhs);
-    
+using TFunctionPtr = std::shared_ptr<TFunction>;
 
+// ------------------------------------------------------------------- Базовые функции -------------------------------------------------------------------
+// Тождественная функция f(x) = x
+class IdentFunction : public TFunction
+{
+public:
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Константная функция f(x) = c
+class ConstFunction : public TFunction
+{
+private:
+    double value_; // Значение константы
+public:
+    explicit ConstFunction(double value); // explicit запрещает неявное преобразование
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Степенная функция f(x) = x^n
+class PowerFunction : public TFunction
+{
+private:
+    int power_; // Показатель степени
+public:
+    explicit PowerFunction(int power);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Экспоненциальная функция f(x) = exp(x)
+class ExpFunction : public TFunction
+{
+public:
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Полиномиальная функция f(x) = a_0 + a_1 * x + a_2 * x^2 + ... + a_n * x^n
+class PolynomialFunction : public TFunction
+{
+private:
+    std::vector<double> coefficients_; // a_0, a_1, ...
+public:
+    explicit PolynomialFunction(const std::vector<double> &coefficients);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// -----------------------------  Составные функции (арифм. операции) ---------------------------------------
+// Функция-сумма: f(x) = g(x) + h(x)
+class AddFunction : public TFunction
+{
+private:
+    TFunctionPtr left_, right_;
+
+public:
+    AddFunction(TFunctionPtr left, TFunctionPtr right);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Функция-разность: f(x) = g(x) - h(x)
+class SubtractFunction : public TFunction
+{
+private:
+    TFunctionPtr left_, right_;
+
+public:
+    SubtractFunction(TFunctionPtr left, TFunctionPtr right);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Функция-произведение: f(x) = g(x) * h(x)
+class MultiplyFunction : public TFunction
+{
+private:
+    TFunctionPtr left_, right_;
+
+public:
+    MultiplyFunction(TFunctionPtr left, TFunctionPtr right);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// Функция-частное: f(x) = g(x) / h(x)
+class DivideFunction : public TFunction
+{
+private:
+    TFunctionPtr left_, right_;
+
+public:
+    DivideFunction(TFunctionPtr left, TFunctionPtr right);
+    double operator()(double x) const override;
+    double GetDeriv(double x) const override;
+    std::string ToString() const override;
+    std::shared_ptr<TFunction> Clone() const override;
+};
+
+// ------------------------------------------ Фабрика ------------------------------------------
+class FunctionFactory
+{
+public:
     /**
-     * Находит корень уравнения f(x) = 0 методом градиентного спуска
-     * func - функция для поиска корня
-     * initial_guess - начальное приближение
-     * iterations - число итераций
+     * Основной метод создания функций
+     * type - тип функции
+     * params - параметры для создания функции
      */
-    double FindRootGradientDescent(const TFunction& func, double initial_guess, int iterations);
-    
+    static TFunctionPtr Create(const std::string &type, const std::vector<double> &params = {});
+
+    // Перегруженные версии для удобства
+    static TFunctionPtr Create(const std::string &type, double param);
+    static TFunctionPtr Create(const std::string &type, int param);
+};
+
+// Перегрузка операторов
+TFunctionPtr operator+(const TFunction &lhs, const TFunction &rhs);
+TFunctionPtr operator-(const TFunction &lhs, const TFunction &rhs);
+TFunctionPtr operator*(const TFunction &lhs, const TFunction &rhs);
+TFunctionPtr operator/(const TFunction &lhs, const TFunction &rhs);
+
+/**
+ * Находит корень уравнения f(x) = 0 методом градиентного спуска
+ * func - функция для поиска корня
+ * initial_guess - начальное приближение
+ * iterations - число итераций
+ */
+double FindRootGradientDescent(const TFunction &func, double initial_guess, int iterations);
+
 #endif
--- functions/test.cpp
+++ functions/test.cpp
@@ -5,19 +5,16 @@
 using ::testing::DoubleNear;
 using ::testing::ThrowsMessage;
 
-// Fixture класс - создает общие данные для группы тестов
 class FunctionLibraryTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        // Выполняется ПЕРЕД КАЖДЫМ тестом
         ident = FunctionFactory::Create("ident");           // f(x) = x
         constant = FunctionFactory::Create("const", 5.0);   // f(x) = 5
         power = FunctionFactory::Create("power", 3);        // f(x) = x³
         exp_func = FunctionFactory::Create("exp");          // f(x) = exp(x)
-        poly = FunctionFactory::Create("polynomial", std::vector<double>{1, 2, 3}); // 1 + 2x + 3x²
+        poly = FunctionFactory::Create("polynomial", std::vector<double>{1, 2, 3}); // 1 + 2x + 3x^2
     }
 
-    // Эти переменные доступны во всех тестах
     TFunctionPtr ident;
     TFunctionPtr constant;
     TFunctionPtr power;
@@ -25,76 +22,75 @@ protected:
     TFunctionPtr poly;
 };
 
-// ТЕСТ 1: Проверка тождественной функции
+// Проверка тождественной функции
 TEST_F(FunctionLibraryTest, CreateIdentFunction) {
-    EXPECT_EQ(ident->ToString(), "x");        // Проверка строкового представления
-    EXPECT_EQ((*ident)(2.0), 2.0);            // Проверка значения в точке x=2
-    EXPECT_EQ(ident->GetDeriv(2.0), 1.0);     // Проверка производной (должна быть 1)
+    EXPECT_EQ(ident->ToString(), "x");        
+    EXPECT_EQ((*ident)(2.0), 2.0);            
+    EXPECT_EQ(ident->GetDeriv(2.0), 1.0);     
 }
 
-// ТЕСТ 2: Проверка константной функции  
+// Проверка константной функции  
 TEST_F(FunctionLibraryTest, CreateConstFunction) {
-    EXPECT_EQ(constant->ToString(), "5");     // Должно быть "5"
-    EXPECT_EQ((*constant)(2.0), 5.0);         // В любой точке значение = 5
-    EXPECT_EQ(constant->GetDeriv(2.0), 0.0);  // Производная константы = 0
+    EXPECT_EQ(constant->ToString(), "5");    
+    EXPECT_EQ((*constant)(2.0), 5.0);         
+    EXPECT_EQ(constant->GetDeriv(2.0), 0.0);  
 }
 
-// ТЕСТ 3: Проверка степенной функции
+// Проверка степенной функции
 TEST_F(FunctionLibraryTest, CreatePowerFunction) {
-    EXPECT_EQ(power->ToString(), "x^3");      // Должно быть "x^3"
-    EXPECT_EQ((*power)(2.0), 8.0);            // 2³ = 8
-    EXPECT_EQ(power->GetDeriv(2.0), 12.0);    // Производная: 3x², при x=2: 3*4=12
+    EXPECT_EQ(power->ToString(), "x^3");      // "x^3"
+    EXPECT_EQ((*power)(2.0), 8.0);
+    EXPECT_EQ(power->GetDeriv(2.0), 12.0);
 }
 
-// ТЕСТ 4: Проверка экспоненты
+// Проверка экспоненты
 TEST_F(FunctionLibraryTest, CreateExpFunction) {
     EXPECT_EQ(exp_func->ToString(), "exp(x)");
-    EXPECT_DOUBLE_EQ((*exp_func)(0.0), 1.0);  // exp(0) = 1
-    EXPECT_DOUBLE_EQ(exp_func->GetDeriv(0.0), 1.0); // Производная exp(x) = exp(x), при 0 = 1
+    EXPECT_DOUBLE_EQ((*exp_func)(0.0), 1.0);
+    EXPECT_DOUBLE_EQ(exp_func->GetDeriv(0.0), 1.0);
 }
 
-// ТЕСТ 5: Проверка полинома
+// Проверка полинома
 TEST_F(FunctionLibraryTest, CreatePolynomialFunction) {
-    EXPECT_EQ(poly->ToString(), "1 + 2*x + 3*x^2"); // Форматирование строки
+    EXPECT_EQ(poly->ToString(), "1 + 2*x + 3*x^2");
     EXPECT_EQ((*poly)(1.0), 6.0);             // 1 + 2*1 + 3*1 = 6
-    EXPECT_EQ(poly->GetDeriv(1.0), 8.0);      // Производная: 2 + 6x, при x=1: 2+6=8
+    EXPECT_EQ(poly->GetDeriv(1.0), 8.0);      // 2 + 6x, при x=1: 2+6=8
 }
 
-// ТЕСТ 6: Проверка обработки ошибок
+// Проверка обработки ошибок
 TEST_F(FunctionLibraryTest, InvalidFunctionTypeThrows) {
-    // Ожидаем исключение при неизвестном типе функции
     EXPECT_THROW(FunctionFactory::Create("unknown"), std::logic_error);
 }
 
-// ТЕСТ 7: Проверка оператора сложения
+// Проверка оператора сложения
 TEST_F(FunctionLibraryTest, AddFunctions) {
     auto sum = *ident + *constant;            // x + 5
     EXPECT_EQ((*sum)(2.0), 7.0);              // 2 + 5 = 7
     EXPECT_EQ(sum->GetDeriv(2.0), 1.0);       // Производная: 1 + 0 = 1
 }
 
-// ТЕСТ 8: Проверка оператора вычитания
+// Проверка оператора вычитания
 TEST_F(FunctionLibraryTest, SubtractFunctions) {
-    auto diff = *power - *constant;           // x³ - 5
+    auto diff = *power - *constant;           // x^3 - 5
     EXPECT_EQ((*diff)(2.0), 3.0);             // 8 - 5 = 3
-    EXPECT_EQ(diff->GetDeriv(2.0), 12.0);     // Производная: 3x² - 0 = 12
+    EXPECT_EQ(diff->GetDeriv(2.0), 12.0);     // 3x^2 - 0 = 12
 }
 
-// ТЕСТ 9: Проверка оператора умножения
+// Проверка оператора умножения
 TEST_F(FunctionLibraryTest, MultiplyFunctions) {
     auto product = *ident * *constant;        // x * 5
     EXPECT_EQ((*product)(2.0), 10.0);         // 2 * 5 = 10
     EXPECT_EQ(product->GetDeriv(2.0), 5.0);   // Производная: 1*5 + x*0 = 5
 }
 
-// ТЕСТ 10: Проверка оператора деления
+// Проверка оператора деления
 TEST_F(FunctionLibraryTest, DivideFunctions) {
-    auto quotient = *power / *constant;       // x³ / 5
+    auto quotient = *power / *constant;       // x^3 / 5
     EXPECT_EQ((*quotient)(2.0), 1.6);         // 8 / 5 = 1.6
-    EXPECT_DOUBLE_EQ(quotient->GetDeriv(2.0), 12.0 / 5.0); // Производная: (3x²)/5 = 12/5
+    EXPECT_DOUBLE_EQ(quotient->GetDeriv(2.0), 12.0 / 5.0); // Производная: (3x^2)/5 = 12/5
 }
 
-// ТЕСТ 11: Проверка сложных выражений
+// Проверка сложных выражений
 TEST_F(FunctionLibraryTest, ComplexExpression) {
     // f(x) = (x² + 2x - 1) * exp(x)
     auto poly2 = FunctionFactory::Create("polynomial", std::vector<double>{-1, 2, 1});
@@ -103,45 +99,43 @@ TEST_F(FunctionLibraryTest, ComplexExpression) {
     EXPECT_DOUBLE_EQ((*complex)(0.0), -1.0);  // (-1 + 0 + 0) * 1 = -1
 }
 
-// ТЕСТ 12: Проверка производных
+// Проверка производных
 TEST_F(FunctionLibraryTest, DerivativeOfPower) {
-    auto power2 = FunctionFactory::Create("power", 2); // x²
+    auto power2 = FunctionFactory::Create("power", 2); // x^2
     EXPECT_EQ(power2->GetDeriv(3.0), 6.0);    // 2x, при x=3: 6
 }
 
-// ТЕСТ 13: Проверка градиентного спуска для линейной функции
+// Проверка градиентного спуска для линейной функции
 TEST_F(FunctionLibraryTest, GradientDescentLinear) {
-    auto linear = FunctionFactory::Create("polynomial", std::vector<double>{-2, 1}); // x - 2
-    double root = FindRootGradientDescent(*linear, 0.0, 100); // Ищем корень x=2
-    EXPECT_NEAR(root, 2.0, 0.1);              // Ожидаем ~2.0 с погрешностью 0.1
+    auto linear = FunctionFactory::Create("polynomial", std::vector<double>{-2, 1}); // f(x) = - 2 + x
+    double root = FindRootGradientDescent(*linear, 0.0, 100);
+    EXPECT_NEAR(root, 2.0, 0.1);           
 }
 
-// ТЕСТ 14: Проверка обработки исключений
+// Проверка обработки исключений
 TEST_F(FunctionLibraryTest, DivisionByZeroThrows) {
     auto zero = FunctionFactory::Create("const", 0.0);
     auto div = *constant / *zero;             // 5 / 0
     
-    EXPECT_THROW((*div)(1.0), std::logic_error);      // Должно бросить исключение
-    EXPECT_THROW(div->GetDeriv(1.0), std::logic_error); // И при вычислении производной
+    EXPECT_THROW((*div)(1.0), std::logic_error);
+    EXPECT_THROW(div->GetDeriv(1.0), std::logic_error); 
 }
 
-// ТЕСТ 15: Проверка из задания
 TEST_F(FunctionLibraryTest, AssignmentExample) {
-    // Пример из задания: f = x², g = 7 + 3x² + 15x³
+    // f = x^2, g = 7 + 3 * x^2 + 15 * x^3
     auto f = FunctionFactory::Create("power", 2);
     auto g = FunctionFactory::Create("polynomial", std::vector<double>{7, 0, 3, 15});
     
-    auto p = *f + *g;                         // x² + (7 + 3x² + 15x³)
+    auto p = *f + *g; // x² + (7 + 3x² + 15x³)
     
     // Проверяем производную в точке x=1
-    // f'(x) = 2x, f'(1) = 2
-    // g'(x) = 6x + 45x², g'(1) = 6 + 45 = 51  
+    // f'(x) = 2x
+    // g'(x) = 6x + 45 * x^2, g'(1) = 51  
     // p'(1) = 2 + 51 = 53
     EXPECT_EQ(p->GetDeriv(1), 53);
 }
 
-// Главная функция тестов
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);   // Инициализация framework
-    return RUN_ALL_TESTS();                    // Запуск всех тестов
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();                
 }
